7f598ab284ee3d778151618324cfeb31acf3f476
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-23 12:28:21 +0100

    slider avec combineLatest

---
+++ src/app/dtos/images-api.dto.ts
@@ -0,0 +1,6 @@
+export interface ImagesApiDto {
+  _id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
--- src/app/rxjs/slider/slider.component.ts
+++ src/app/rxjs/slider/slider.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from "@angular/core";
-import { Observable, map, startWith, timer } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, Input } from "@angular/core";
+import { Observable, combineLatest, map, startWith, timer } from "rxjs";
+import { API } from "../../../config/api.config";
+import { ImagesApiDto } from "../../dtos/images-api.dto";
 
 @Component({
   selector: "app-slider",
@@ -16,11 +19,21 @@ export class SliderComponent {
     "rotating_card_profile2.png",
     "rotating_card_profile3.png",
   ];
-
+  http = inject(HttpClient);
+  images$ = this.http.get<ImagesApiDto[]>(API.photos);
   /* Todo : Créer le flux permettant de générer les images à afficher dans le slider */
-  paths$: Observable<string> = timer(0, this.timer).pipe(
-    // 0 1 2 3 4 5 6
-    map((index) => this.imagePaths[index % this.imagePaths.length])
-    // img1 img2 ....
+  paths$: Observable<string> = combineLatest([
+    timer(0, this.timer),
+    this.images$,
+  ]).pipe(
+    map(([index, images]) => images[index % images.length].thumbnailUrl),
+    startWith(
+      "https://logowik.com/content/uploads/images/angular-a-letter-new-20243216.logowik.com.webp"
+    )
   );
+  //.pipe(map(() => ))
+  // [O, ImagesApiDto[]],  [1, ImagesApiDto[]]
+  // 0 1 2 3 4 5 6
+  //map((index) => this.imagePaths[index % this.imagePaths.length])
+  // img1 img2 ....
 }
