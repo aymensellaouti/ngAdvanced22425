a1879fa2683d8cb082d08c627f97f37fea5f190c
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-23 14:34:36 +0100

    mise en place exercice auth with subject

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,25 +1,39 @@
-import { Injectable } from '@angular/core';
-import { CredentialsDto } from '../dto/credentials.dto';
-import { LoginResponseDto } from '../dto/login-response.dto';
-import { HttpClient } from '@angular/common/http';
-import { API } from '../../../config/api.config';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { CredentialsDto } from "../dto/credentials.dto";
+import { LoginResponseDto } from "../dto/login-response.dto";
+import { HttpClient } from "@angular/common/http";
+import { API } from "../../../config/api.config";
+import { Observable, Subject } from "rxjs";
 
+export interface ConnectedUser {
+  id: string;
+  email: string;
+}
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  user$ = new Subject<ConnectedUser | null>();
+  isLoggedIn$!: Observable<boolean>;
+  isLoggedOut$!: Observable<boolean>;
+  constructor(private http: HttpClient) {
+    // Todo Récupérer le user du localstorage et faire le nécessaire
+  }
 
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
+    // Todo Déplacer le stockage du token
+    // Todo ajoute le user au flux
+    // Todo ajouter le user dans le localStorage
     return this.http.post<LoginResponseDto>(API.login, credentials);
   }
 
   isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
+    return !!localStorage.getItem("token");
   }
 
   logout() {
-    localStorage.removeItem('token');
+    // Todo enlever le user du localstorage
+    // Todo mettre à jour le flux
+    localStorage.removeItem("token");
   }
 }
