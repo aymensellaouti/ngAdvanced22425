3fd83012325c7b49cf36169087bc57734980bd64
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-23 15:28:15 +0100

    user behaviourSubject

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,28 +3,43 @@ import { CredentialsDto } from "../dto/credentials.dto";
 import { LoginResponseDto } from "../dto/login-response.dto";
 import { HttpClient } from "@angular/common/http";
 import { API } from "../../../config/api.config";
-import { Observable, Subject } from "rxjs";
+import { BehaviorSubject, map, Observable, Subject, tap } from "rxjs";
 
 export interface ConnectedUser {
-  id: string;
+  id: number;
   email: string;
 }
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  user$ = new Subject<ConnectedUser | null>();
-  isLoggedIn$!: Observable<boolean>;
-  isLoggedOut$!: Observable<boolean>;
+  private userSubject$ = new BehaviorSubject<ConnectedUser | null>(null);
+  user$ = this.userSubject$.asObservable();
+  isLoggedIn$: Observable<boolean> = this.user$.pipe(map((user) => !!user));
+  isLoggedOut$: Observable<boolean> = this.user$.pipe(map((user) => !user));
   constructor(private http: HttpClient) {
     // Todo Récupérer le user du localstorage et faire le nécessaire
+    const user = localStorage.getItem("user");
+    if (user) {
+      this.userSubject$.next(JSON.parse(user));
+    }
   }
 
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
-    // Todo Déplacer le stockage du token
-    // Todo ajoute le user au flux
-    // Todo ajouter le user dans le localStorage
-    return this.http.post<LoginResponseDto>(API.login, credentials);
+    return this.http.post<LoginResponseDto>(API.login, credentials).pipe(
+      tap((response) => {
+        // Todo Déplacer le stockage du token : OK
+        localStorage.setItem("token", response.id);
+        // Todo ajoute le user au flux
+        const user: ConnectedUser = {
+          id: response.userId,
+          email: credentials.email,
+        };
+        this.userSubject$.next(user);
+        // Todo ajouter le user dans le localStorage
+        localStorage.setItem("user", JSON.stringify(user));
+      })
+    );
   }
 
   isAuthenticated(): boolean {
@@ -33,7 +48,9 @@ export class AuthService {
 
   logout() {
     // Todo enlever le user du localstorage
+    localStorage.removeItem("user");
     // Todo mettre à jour le flux
+    this.userSubject$.next(null);
     localStorage.removeItem("token");
   }
 }
