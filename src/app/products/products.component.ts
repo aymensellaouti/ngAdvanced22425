1c894b7d0457865a5384abf622547490e3f3b1a9
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-23 15:52:44 +0100

    product sprint 1

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import {
   BehaviorSubject,
   Observable,
@@ -17,9 +17,18 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent {
+  productService = inject(ProductService);
+  private settings$ = new BehaviorSubject<Settings>({
+    skip: 0,
+    limit: 12,
+  });
   // Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   // Todo Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
+  products$: Observable<Product[]> = this.settings$.pipe(
+    // {0 12}, {12 12}, {24 12} ....
+    concatMap((setting) => this.productService.getProducts(setting))
+    // apiResponse1, apiReponse2, ....
+  );
   constructor() {}
 
   more() {}
