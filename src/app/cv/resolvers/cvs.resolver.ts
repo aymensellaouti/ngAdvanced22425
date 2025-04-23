b8be51238afa526564363f92acf1d25a34b9330f
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-23 12:31:51 +0100

    catchError avec le resolver

--- src/app/cv/resolvers/cvs.resolver.ts
+++ src/app/cv/resolvers/cvs.resolver.ts
@@ -2,8 +2,18 @@ import { ResolveFn } from "@angular/router";
 import { CvService } from "./../services/cv.service";
 import { inject } from "@angular/core";
 import { Cv } from "../model/cv";
+import { catchError, of } from "rxjs";
+import { ToastrService } from "ngx-toastr";
 
 export const cvsResolver: ResolveFn<Cv[]> = (route, state) => {
   const cvService = inject(CvService);
-  return cvService.getCvs();
+  const toastr = inject(ToastrService);
+  return cvService.getCvs().pipe(
+    catchError((e) => {
+      toastr.error(`
+          Attention!! Les données sont fictives, problème avec le serveur.
+          Veuillez contacter l'admin.`);
+      return of(cvService.getFakeCvs());
+    })
+  );
 };
