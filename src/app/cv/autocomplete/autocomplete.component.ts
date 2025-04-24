30a5b4ae2ab950d589c11a0dd37d8aba6fb2b586
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-24 09:39:34 +0100

    autocomplete optimisé

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -1,7 +1,15 @@
 import { Component, inject } from "@angular/core";
 import { FormBuilder, AbstractControl } from "@angular/forms";
-import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  Observable,
+  switchMap,
+  tap,
+} from "rxjs";
 import { CvService } from "../services/cv.service";
+import { Cv } from "../model/cv";
 
 @Component({
   selector: "app-autocomplete",
@@ -15,9 +23,14 @@ export class AutocompleteComponent {
     return this.form.get("search")!;
   }
   form = this.formBuilder.group({ search: [""] });
-  constructor() {
-    this.search.valueChanges.subscribe((val) => {
-      console.log(val);
-    });
-  }
+  cvs$: Observable<Cv[]> = this.search.valueChanges.pipe(
+    // ma teb3ethli valeur kan ma io93ed le flux stable
+    // 500 ms
+    debounceTime(500),
+    // ma teb3athlich el valuer kan ma tetbadelch
+    distinctUntilChanged(),
+    filter((search) => search && search.length >= 3),
+    switchMap((searchField) => this.cvService.selectByName(searchField))
+  );
+  constructor() {}
 }
