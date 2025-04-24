6fdb5b53835f893b4e165a2a92bd6f808beaad70
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-24 13:28:32 +0100

    saveForm if valid

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnDestroy } from "@angular/core";
 import {
   AbstractControl,
   FormBuilder,
@@ -11,13 +11,14 @@ import { Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
 import { APP_ROUTES } from "src/config/routes.config";
 import { Cv } from "../model/cv";
+import { CONSTANTES } from "../../../config/const.config";
 
 @Component({
   selector: "app-add-cv",
   templateUrl: "./add-cv.component.html",
   styleUrls: ["./add-cv.component.css"],
 })
-export class AddCvComponent {
+export class AddCvComponent implements OnDestroy {
   formBuilder = inject(FormBuilder);
   form: FormGroup = this.formBuilder.group(
     {
@@ -53,13 +54,42 @@ export class AddCvComponent {
     private cvService: CvService,
     private router: Router,
     private toaster: ToastrService
-  ) {}
+  ) {
+    this.age.valueChanges.subscribe({
+      next: (age) => {
+        if (age < 18) {
+          this.path?.disable();
+        } else {
+          this.path?.enable();
+        }
+      },
+    });
+    const savedForm = localStorage.getItem(CONSTANTES.addCvToken);
+    if (savedForm) {
+      this.form.patchValue(JSON.parse(savedForm));
+    }
+  }
+  ngOnDestroy(): void {
+    if (this.form.valid) {
+      localStorage.setItem(
+        CONSTANTES.addCvToken,
+        JSON.stringify(this.form.value)
+      );
+    }
+  }
 
   addCv() {
+    const cv = this.form.value;
+    if (!cv.path) {
+      cv.path = "";
+    }
+
     this.cvService.addCv(this.form.value).subscribe({
       next: () => {
         this.toaster.success(`Le cv a été ajouté avec succès`);
         this.router.navigate([APP_ROUTES.cv]);
+        localStorage.removeItem(CONSTANTES.addCvToken);
+        this.form.reset();
       },
       error: (erreur) => {
         console.log(erreur);
--- src/config/const.config.ts
+++ src/config/const.config.ts
@@ -1,4 +1,5 @@
 export const CONSTANTES = {
   defaultImage: "cv.png",
   env: "production",
+  addCvToken: "addCvToken",
 };
